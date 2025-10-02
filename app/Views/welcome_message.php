82708af2e5c76a4b5d7d7cda472a9ed2eee0aaae
Glenn-Rhee <glennviktor5@gmail.com>
2025-10-02 09:07:54 +0700

    feat: fixed layouts

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,27 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend('layouts/main') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
-    <meta name="description" content="The small framework with powerful features">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-    <link rel="stylesheet" href="<?= base_url('css/output.css') ?>">
-</head>
+<?= $this->section('title') ?>
+Product Page
+<?= $this->endSection() ?>
 
-<body>
+<?= $this->section('content') ?>
 
-    <marquee behavior="" direction="left">
-        <h1 class="text-4xl font-bold text-red-900">Hello world</h1>
-    </marquee>
-    <details class="dropdown">
-        <summary class="btn m-1">open or close</summary>
-        <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-        </ul>
-    </details>
-</body>
-
-</html>
+<marquee behavior="" direction="left">
+    <h1 class="text-4xl font-bold text-red-900">Hello world</h1>
+</marquee>
+<details class="dropdown">
+    <summary class="btn m-1">open or close</summary>
+    <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+    </ul>
+</details>
+<?= $this->endSection() ?>
