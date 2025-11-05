fb9b1704a5e98653ca13bc6dc9d8a16c8449d9ae
Glenn-Rhee <glennviktor5@gmail.com>
2025-11-05 17:47:33 +0700

    feat: added logger prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,27 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: [
+    { emit: "event", level: "query" },
+    { emit: "event", level: "info" },
+    { emit: "event", level: "warn" },
+    { emit: "event", level: "error" },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("Query:", e.query);
+  console.log("Duration:", e.duration);
+});
+
+prisma.$on("info", (e) => {
+  console.log("Info:", e.message);
+});
+
+prisma.$on("warn", (e) => {
+  console.log("Warning:", e.message);
+});
+
+prisma.$on("error", (e) => {
+  console.log("❌ Error:", e.message);
+});
