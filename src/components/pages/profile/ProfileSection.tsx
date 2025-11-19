abf9fd046123a4db405de9456d6e6c9974667b32
Glenn-Rhee <glennviktor5@gmail.com>
2025-11-19 09:53:02 +0700

    fix: fixed imageurl

--- src/components/pages/profile/ProfileSection.tsx
+++ src/components/pages/profile/ProfileSection.tsx
@@ -1,7 +1,12 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Separator } from "@/components/ui/separator";
 import { DataUser } from "@/types";
-import { IconLocation, IconMail, IconPhone } from "@tabler/icons-react";
+import {
+  IconLocation,
+  IconMail,
+  IconPhone,
+  IconUserFilled,
+} from "@tabler/icons-react";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 
@@ -16,16 +21,22 @@ export default async function ProfileSection(props: ProfileSectionProps) {
   return (
     <div className="bg-white max-w-md w-full rounded-2xl p-8 shadow-xl border-2 border-orange-100">
       <div className="w-full flex items-center flex-col justify-center">
-        <Image
-          src={data.image}
-          alt="Profile Ariel Rizki"
-          width={150}
-          height={100}
-          className="aspect-square object-cover rounded-full"
-        />
+        {data.image === "" ? (
+          <div className="aspect-square rounded-full shadow-md bg-orange-100 w-32 h-32 flex items-center justify-center">
+            <IconUserFilled className="text-orange-800" size={80} />
+          </div>
+        ) : (
+          <Image
+            src={data.image}
+            alt="Profile Ariel Rizki"
+            width={150}
+            height={100}
+            className="aspect-square object-cover rounded-full"
+          />
+        )}
         <div className="flex flex-col w-full items-center mt-2">
           <span className="font-semibold text-2xl text-gray-800">
-            {data.fullname}
+            {data.fullname || "-"}
           </span>
           <span className="text-sm font-medium text-gray-500">
             {session?.user.id?.slice(0, 8)} | {session?.user.role}
