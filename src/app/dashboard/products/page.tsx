aba2a677bdef2ab71fd7b50ffd05076861534eb8
Glenn-Rhee <glennviktor5@gmail.com>
2025-11-22 11:59:49 +0700

    refactor: make a layout for dashboard page

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,31 +1,14 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-
 export default function ProductsDashboardPage() {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
-              <h1 className="text-3xl font-bold">Products Dashboard</h1>
-              <div className="grid gap-4">
-                <p>Ini halaman Products Dashboard</p>
-              </div>
-            </div>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
+          <h1 className="text-3xl font-bold">Products Dashboard</h1>
+          <div className="grid gap-4">
+            <p>Ini halaman Products Dashboard</p>
           </div>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+    </div>
   );
 }
