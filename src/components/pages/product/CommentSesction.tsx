1cd7fe44f6895b1d044706910d9e94607a2f03c0
Glenn-Rhee <glennviktor5@gmail.com>
2025-12-02 12:54:58 +0700

    feat: added funtionallity at commentsection component

--- src/components/pages/product/CommentSesction.tsx
+++ src/components/pages/product/CommentSesction.tsx
@@ -1,41 +1,51 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 import { IconStarFilled, IconUserFilled } from "@tabler/icons-react";
+import { useState } from "react";
 
+const data = [
+  {
+    name: "Glenn Rhee",
+    commentAt: "7 Month Ago",
+    stars: 5,
+  },
+  {
+    name: "Glenn Rhee",
+    commentAt: "7 Month Ago",
+    stars: 4,
+  },
+  {
+    name: "Glenn Rhee",
+    commentAt: "7 Month Ago",
+    stars: 3,
+  },
+  {
+    name: "Glenn Rhee",
+    commentAt: "7 Month Ago",
+    stars: 2,
+  },
+  {
+    name: "Glenn Rhee",
+    commentAt: "7 Month Ago",
+    stars: 1,
+  },
+];
 export default function CommentSesction() {
-  const data = [
-    {
-      name: "Glenn Rhee",
-      commentAt: "7 Month Ago",
-      stars: 5,
-    },
-    {
-      name: "Glenn Rhee",
-      commentAt: "7 Month Ago",
-      stars: 4,
-    },
-    {
-      name: "Glenn Rhee",
-      commentAt: "7 Month Ago",
-      stars: 3,
-    },
-    {
-      name: "Glenn Rhee",
-      commentAt: "7 Month Ago",
-      stars: 2,
-    },
-    {
-      name: "Glenn Rhee",
-      commentAt: "7 Month Ago",
-      stars: 1,
-    },
-  ];
+  const [rating, setRating] = useState<number>(0);
+  const [comment, setComment] = useState<string>("");
+
+  async function handleComment() {
+    console.log("Comment: ", comment);
+    console.log("Rating: ", rating);
+  }
   return (
     <section className="max-w-6xl w-full mx-auto mt-8 flex gap-x-4 px-4 mb-8">
       <div className="w-full flex-1 h-fit sticky top-[6rem] bg-white border px-4 py-3 border-gray-300 rounded-sm shadow-sm">
-        <h4 className="text-stone-900 font-semibold text-xl">Filter Ulasan</h4>
+        <h4 className="text-stone-900 font-semibold text-xl">Filter Reviews</h4>
         <Separator className="bg-stone-700/40 my-3" />
         <h4 className="text-stone-900 font-medium text-lg">Rating</h4>
         <ul className="flex flex-col gap-y-4 mt-2">
@@ -79,20 +89,41 @@ export default function CommentSesction() {
           <div className="flex flex-col gap-y-2 w-full">
             <input
               type="text"
+              value={comment}
+              onKeyUp={(e) =>
+                e.key.toLowerCase() === "enter" && handleComment()
+              }
+              onChange={(e) => setComment(e.target.value)}
               className="border-b border-gray-500/40 w-full placeholder:text-stone-500 transition-colors text-stone-800 pb-2 placeholder:text-sm focus:border-b focus:outline-none focus:border-gray-600/70"
               placeholder="Add your comment..."
             />
             <div className="flex items-center w-full justify-between gap-x-2">
               <div className="flex items-center gap-x-2">
                 {Array.from({ length: 5 }).map((_, i) => (
-                  <IconStarFilled
-                    size={18}
+                  <button
                     key={i}
-                    className="text-gray-500 transition-colors duration-200 hover:text-yellow-500 cursor-pointer"
-                  />
+                    type="button"
+                    onClick={() => setRating(i + 1)}
+                  >
+                    <IconStarFilled
+                      size={18}
+                      className={cn(
+                        "transition-colors duration-200 cursor-pointer text-gray-500",
+                        {
+                          "text-gray-500": rating === 0,
+                          "text-yellow-500": i + 1 <= rating,
+                        }
+                      )}
+                    />
+                  </button>
                 ))}
               </div>
-              <Button className="cursor-pointer" size={"sm"}>
+              <Button
+                onClick={handleComment}
+                type="submit"
+                className="cursor-pointer"
+                size={"sm"}
+              >
                 Comment
               </Button>
             </div>
